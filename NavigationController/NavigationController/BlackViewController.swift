e14dbdfb3b8b10ac258ca458d8a3a67cff476aee
NguyenVanCong9x <90688506+NguyenVanCong9x@users.noreply.github.com>
2022-01-08 17:35:32 +0700

    NavigationControllerInStoryBoard

---
+++ NavigationController/NavigationController/BlackViewController.swift
@@ -0,0 +1,29 @@
+//
+//  BlackViewController.swift
+//  NavigationController
+//
+//  Created by Nguyen Van Cong on 1/8/22.
+//
+
+import UIKit
+
+class BlackViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = "Black Screen"
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
